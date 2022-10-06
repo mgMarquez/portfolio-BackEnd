c68ae5cce1cbc6567d40c879794dc19b9b83d993
Mauricio Marquez <mgmarquezsilva@gmail.com>
2022-10-06 20:59:58 -0300

    agregada dependencia ModelMapper

--- portfolio-backend/src/main/java/com/yoprogramo/portfoliobackend/PortfolioBackendApplication.java
+++ portfolio-backend/src/main/java/com/yoprogramo/portfoliobackend/PortfolioBackendApplication.java
@@ -1,11 +1,17 @@
 package com.yoprogramo.portfoliobackend;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PortfolioBackendApplication {
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioBackendApplication.class, args);
 	}
