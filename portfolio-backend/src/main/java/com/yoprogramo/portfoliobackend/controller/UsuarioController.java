be444cec968f5704af85e6b7b1f5fb9e0b2da0d1
mgMarquez <mgmarquezsilva@gmail.com>
2022-10-22 02:21:27 -0300

    path actualizado

--- portfolio-backend/src/main/java/com/yoprogramo/portfoliobackend/controller/UsuarioController.java
+++ portfolio-backend/src/main/java/com/yoprogramo/portfoliobackend/controller/UsuarioController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/usuario")
+@RequestMapping("api/usuarios")
 public class UsuarioController {
     @Autowired
     private IUsuarioService service;
@@ -23,7 +23,7 @@ public class UsuarioController {
         return service.findUsuarioById(id);
     }
 
-    @PostMapping("/new")
+    @PostMapping("/")
     public UsuarioDTO addUsuario(@RequestBody UsuarioDTO usuarioDTO) {
         return service.saveUsuario(usuarioDTO);
     }
