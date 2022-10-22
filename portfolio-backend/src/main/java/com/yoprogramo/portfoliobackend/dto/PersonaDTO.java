810acaf7f70683a2d1282f1eba9c6485bca8fe06
mgMarquez <mgmarquezsilva@gmail.com>
2022-10-21 21:08:21 -0300

    Refactor persona

---
+++ portfolio-backend/src/main/java/com/yoprogramo/portfoliobackend/dto/PersonaDTO.java
@@ -0,0 +1,25 @@
+package com.yoprogramo.portfoliobackend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter @Setter
+public class PersonaDTO {
+    private String nombre;
+    private String apellido;
+    private LocalDate fechaNac;
+    private String profesion;
+    private String nacionalidad;
+    private String ubicacion;
+    private String acercaDe;
+    private String email;
+    private String telefono;
+    private String fotoUrl;
+    private String bannerUrl;
+    private Long usuario_id;
+    private List<EducacionDTO> educaciones = new ArrayList<>();
+}
