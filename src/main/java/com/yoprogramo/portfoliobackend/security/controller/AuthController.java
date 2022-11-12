d0061772363211f6a6aeff733a82eca3566ba24e
mgMarquez <mgmarquezsilva@gmail.com>
2022-11-11 23:10:41 -0300

    arreglando error cors

--- src/main/java/com/yoprogramo/portfoliobackend/security/controller/AuthController.java
+++ src/main/java/com/yoprogramo/portfoliobackend/security/controller/AuthController.java
@@ -27,7 +27,7 @@ import java.util.List;
 import java.util.Set;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/auth")
 public class AuthController {
     @Autowired
