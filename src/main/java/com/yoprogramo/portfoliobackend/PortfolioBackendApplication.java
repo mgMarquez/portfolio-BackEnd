523b8ac23cd0131c17bbd5ac75be3e1a7016f969
mgMarquez <mgmarquezsilva@gmail.com>
2022-11-13 17:56:16 -0300

    CORS mapping global

--- src/main/java/com/yoprogramo/portfoliobackend/PortfolioBackendApplication.java
+++ src/main/java/com/yoprogramo/portfoliobackend/PortfolioBackendApplication.java
@@ -4,6 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PortfolioBackendApplication {
@@ -18,4 +20,13 @@ public class PortfolioBackendApplication {
 		SpringApplication.run(PortfolioBackendApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+			}
+		};
+	}
 }
