fab26ab830f45abbdc666d3369a6b28b3532305c
mgMarquez <mgmarquezsilva@gmail.com>
2022-11-13 16:35:57 -0300

    CORS global y variables en application.properties

--- src/main/java/com/yoprogramo/portfoliobackend/security/controller/AuthController.java
+++ src/main/java/com/yoprogramo/portfoliobackend/security/controller/AuthController.java
@@ -26,7 +26,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
