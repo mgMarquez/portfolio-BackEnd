66a791c123ca68d714198ec36698d7da70dbf938
mgMarquez <mgmarquez@gmail.com>
2023-01-07 16:38:28 -0300

    agregada la url del frontend

--- src/main/java/com/yoprogramo/portfoliobackend/PortfolioBackendApplication.java
+++ src/main/java/com/yoprogramo/portfoliobackend/PortfolioBackendApplication.java
@@ -25,7 +25,7 @@ public class PortfolioBackendApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200", "https://hosting-angular-c1a9a.firebaseapp.com/login");
 			}
 		};
 	}
